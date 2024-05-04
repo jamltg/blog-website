da0cf8a2d1d2266d7c2ab40bf2ba60019e72029e
jamltg <jamletigio@gmail.com>
2024-05-04 15:52:58 +0800

    Connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.CONNECTION_STRING)
+.then(()=>console.log('MongoDB successful'))
+.catch((err)=>console.log(err))
 
 const app = express();
+
 
 app.listen(3000, ()=>{
   console.log('Server is running in port 3000')
 })
+
+
+//lvdEyVW5mHyNvjC9
+
+
+//mongodb+srv://jamletigio:lvdEyVW5mHyNvjC9@blog-website.xdo3bl9.mongodb.net/?retryWrites=true&w=majority&appName=blog-website
