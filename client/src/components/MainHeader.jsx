1de373c9280e26d1eb57e5ddb37ec3989e3be293
jamltg <jamletigio@gmail.com>
2024-05-08 16:46:26 +0800

    update header with user data

--- client/src/components/MainHeader.jsx
+++ client/src/components/MainHeader.jsx
@@ -1,18 +1,22 @@
 import { Link } from 'react-router-dom'
 import { Pivot as Hamburger } from 'hamburger-react'
+import { useSelector } from 'react-redux';
+import { Avatar, Dropdown } from 'flowbite-react';
 
 // eslint-disable-next-line react/prop-types
 export default function MainHeader({toggleMenu, isOpen}) {
+  const { currentUser } = useSelector(state => state.user);
+
   return (
     <>
-      <Link to="/">
+            <Link to="/">
               <h1 className="text-3xl font-semibold">Jam&apos;s Blog</h1>
             </Link>
             <div>
               <h1>Search</h1>
             </div>
             <div className='flex items-center'>
-              <ul className='flex'>
+              <ul className='hidden lg:flex'>
                 <li className="p-4">
                   <Link to="/">Home</Link>
                 </li>
@@ -23,11 +27,35 @@ export default function MainHeader({toggleMenu, isOpen}) {
                   <Link to="/resources">Resources</Link>
                 </li>
               </ul>
-              <div className='hidden md:block'>
-                <Link to="/sign-in"><button className='border-2 px-4 py-2 rounded-lg'>Sign In</button></Link>
+              <div>
+                {currentUser ?(
+                  <Dropdown
+                    arrowIcon={false}
+                    inline
+                    label={
+                      <Avatar
+                        alt="user"
+                        img={currentUser.profilePicture}
+                        rounded
+                      />
+                    }
+                  >
+                    <Dropdown.Header>
+                      <span className='block text-sm'>@{currentUser.username}</span>
+                      <span className='block text-sm font-medium truncate mt-1'>{currentUser.email}</span>
+                    </Dropdown.Header>
+                    <Link to={'/dashboard?tab=profile'}>
+                      <Dropdown.Item>Profile</Dropdown.Item>
+                    </Link>
+                    <Dropdown.Divider/>
+                    <Dropdown.Item>Sign Out</Dropdown.Item>
+                  </Dropdown>
+                ):(
+                  <Link to="/sign-in"><button className='border-2 px-4 py-2 rounded-lg'>Sign In</button></Link>
+                )}
               </div>
             </div>
-            <div className="md:hidden">
+            <div className="lg:hidden">
                 <Hamburger toggled={isOpen} toggle={toggleMenu} size={20}/>
             </div>
     </>
